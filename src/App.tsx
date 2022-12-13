585a927677b67d6dbb5db5410d20f77e1eb880a3
esau-morais <esaumorais7@gmail.com>
2022-12-13 10:38:15 -0300

    fix: update type-safety, seo, project name and more

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const schema = z.object({
 type Schema = z.infer<typeof schema>
 
 const App = (): JSX.Element => {
-  const [suggestion, setSuggestion] = useState<string| undefined>('')
+  const [suggestion, setSuggestion] = useState<string>('')
   const methods = useForm<Schema>({
     resolver: zodResolver(schema)
   })
@@ -38,10 +38,16 @@ const App = (): JSX.Element => {
         presence_penalty: 0
       })
 
-      if (!result.data.choices?.[0].text) return new Error('Invalid response')
+      if (
+        result.data.choices?.[0].text === null ||
+        result.data.choices?.[0].text === undefined
+      ) {
+        return new Error('Invalid response')
+      }
       const suggestion = result.data.choices[0].text
 
       setSuggestion(suggestion)
+      return suggestion
     } catch (error) {
       if (error instanceof Error) {
         return new Error('Error => ', error)
@@ -57,12 +63,16 @@ const App = (): JSX.Element => {
     <FormProvider {...methods}>
       <form
         className='max-w-6xl mx-auto min-h-screen flex flex-col items-center mt-32 mb-16 sm:text-center sm:mb-0'
-        onSubmit={formSubmit(handleSubmit) as any}
+        onSubmit={formSubmit(handleSubmit)}
       >
         <div className='w-12 h-12'>
-          <img src='https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Ficon-icons.com%2Ficons2%2F931%2FPNG%2F512%2Fpencil_icon-icons.com_72386.png&amp;f=1&amp;nofb=1' />
+          <img
+            src='https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Ficon-icons.com%2Ficons2%2F931%2FPNG%2F512%2Fpencil_icon-icons.com_72386.png&amp;f=1&amp;nofb=1'
+            alt="Pencil icon"
+            loading="lazy"
+          />
         </div>
-        <div className='text-gray-400 text-center'>
+        <div className='text-neutral-500 text-center'>
           Combine &quot;very&quot; with a simple adjective and get a more concise adjective
         </div>
         <div className='flex flex-col space-y-6 md:grid md:grid-cols-12 justify-center items-center w-full mb-4 py-16'>
@@ -71,7 +81,7 @@ const App = (): JSX.Element => {
           <input
             className={cx(
               'md:col-span-4 text-center border-b-2 font-sans text-xl sm:text-4xl lg:text-5xl xl:text-6xl font-bold pb-6 transition duration-200 bg-white appearance-none focus:outline-none',
-              errors.adjective ? 'border-red-500' : 'border-gray-600'
+              errors.adjective !== undefined ? 'border-red-500' : 'border-gray-600'
             )}
             placeholder='boring'
             {...register('adjective')}
@@ -82,15 +92,14 @@ const App = (): JSX.Element => {
           <p
             className={cx(
               'cursor-pointer text-center text-xl sm:text-4xl lg:text-5xl xl:text-6xl font-bold',
-              suggestion !== '' ? 'text-green-700' : 'text-gray-500',
+              suggestion.trim() !== '' ? 'text-green-700' : 'text-gray-500',
               'font-serif'
             )}>
             {suggestion ?? 'tedious'}
           </p>
         </div>
           <div className='text-center  w-96 col-span-4'>
-            <p
-              className={'cursor-pointer text-center text-xl sm:text-4xl lg:text-5xl xl:text-6xl font-bold font-serif'}>
+            <p className='cursor-pointer text-center text-xl sm:text-4xl lg:text-5xl xl:text-6xl font-bold font-serif'>
             </p>
           </div>
         </div>
